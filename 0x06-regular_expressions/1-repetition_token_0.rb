7d5a8f8a068ec487a3f936933749d3209ae46d24
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-11-28 17:41:23 -0800

    0x06 Regular Expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,21 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join
+# 1-repetition_token_0.rb
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: ruby 1-repetition_token_0.rb <input_string>"
+  exit(1)
+end
+
+# Get the input string from the command line argument
+input_string = ARGV[0]
+
+# Define the regular expression
+pattern = /^mandatory$/
+
+# Match the input string against the pattern
+if input_string.match?(pattern)
+  puts "Input string '#{input_string}' matches the pattern."
+else
+  puts "Input string '#{input_string}' does not match the pattern."
+end
+
