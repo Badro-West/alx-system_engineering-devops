68fac0946d549c6d54e84f5d9befa5f9271b8929
Badro-West <badreddine.mezgueldi5@gmail.com>
2024-01-19 17:58:37 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# 1-install_a_package.pp
+
+# Define the package resource for Flask
+package { 'Flask':
+  ensure   => '2.1.0',  # Ensure the specified version is installed
+  provider => 'pip3',   # Use pip3 as the provider
 }
+
+# Notify that the package is installed
+notify { 'Flask installed':
+  require => Package['Flask'],
+}
+
